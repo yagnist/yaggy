e0bc2621d570b0fe8a5e7d6e0bb5002787d9124a
Yuri Egorov <ysegorov@gmail.com>
2020-03-04 08:36:59 +0300

    [logging] switch to self-made colorize function

--- yaggy/logging.py
+++ yaggy/logging.py
@@ -1,10 +1,66 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import logging
 import logging.config
 
-import click
+
+# NB. see https://en.wikipedia.org/wiki/ANSI_escape_code
+# styles bold:1 dim:2 italic:3 underline:4 inverse:7 hidden:8 strikethrough:9
+FG = {
+    'black': 30,
+    'red': 31,
+    'green': 32,
+    'yellow': 33,
+    'blue': 34,
+    'magenta': 35,
+    'cyan': 36,
+    'white': 37,
+    'bright_black': 90,
+    'bright_red': 91,
+    'bright_green': 92,
+    'bright_yellow': 93,
+    'bright_blue': 94,
+    'bright_magenta': 95,
+    'bright_cyan': 96,
+    'bright_white': 97,
+}
+BG = {
+    'black': 40,
+    'red': 41,
+    'green': 42,
+    'yellow': 43,
+    'blue': 44,
+    'magenta': 45,
+    'cyan': 46,
+    'white': 47,
+    'bright_black': 100,
+    'bright_red': 101,
+    'bright_green': 102,
+    'bright_yellow': 103,
+    'bright_blue': 104,
+    'bright_magenta': 105,
+    'bright_cyan': 106,
+    'bright_white': 107,
+}
+
+
+def colorize(text, fg=None, bg=None):
+    clr = '\x1b['
+    reset = '\x1b[0m'
+    spec = []
+
+    if bg in BG:
+        spec.append(f'{BG[bg]}')
+    if fg in FG:
+        spec.append(f'{FG[fg]}')
+
+    if spec:
+        spec = ';'.join(spec)
+        text = f'{clr}{spec}m{text}{reset}'
+
+    return text
 
 
 class IndentedFormatter(logging.Formatter):
@@ -49,10 +105,13 @@ class ColoredStreamHandler(logging.StreamHandler):
         try:
             msg = self.format(record)
             err = record.levelno in (logging.ERROR, logging.CRITICAL)
+            stream = sys.stderr if err else sys.stdout
+            self.setStream(stream)
 
             # NB. just empty line in log
             if not getattr(record, 'message'):
-                click.echo('', err=err)
+                stream.write(self.terminator)
+                self.flush()
                 return
             if hasattr(self.formatter, 'indent'):
                 width = len(self.formatter.indent)
@@ -69,18 +128,19 @@ class ColoredStreamHandler(logging.StreamHandler):
                     body_color = self.COLOR_COMMENT if is_comment else None
 
                     head = (
-                        click.style(left + leftsep, fg=fg) +
-                        click.style(middle, fg=clr) +
-                        click.style(rightsep, fg=fg) +
-                        click.style(right, fg=body_color))
+                        colorize(left + leftsep, fg=fg) +
+                        colorize(middle, fg=clr) +
+                        colorize(rightsep, fg=fg) +
+                        colorize(right, fg=body_color))
 
                 tail = ''.join(
-                    click.style(x[:width], fg=fg) +
-                    click.style(x[width:], fg=body_color)
+                    colorize(x[:width], fg=fg) +
+                    colorize(x[width:], fg=body_color)
                     for x in tail
                 )
                 msg = head + tail
-            click.echo(msg, err=err)
+            stream.write(msg + self.terminator)
+            self.flush()
         except RecursionError:
             raise
         except Exception:
