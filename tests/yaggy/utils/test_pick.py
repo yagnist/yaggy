3e1a7554371639ddcefcb9a73303492170f46abd
Yuri Egorov <ysegorov@gmail.com>
2020-04-29 10:22:28 +0300

    [tests] tune test for yaggy.utils:pick

--- tests/yaggy/utils/test_pick.py
+++ tests/yaggy/utils/test_pick.py
@@ -2,28 +2,28 @@
 
 from collections import namedtuple
 
-from yaggy import utils as u
+from yaggy.utils import pick
 
 
-def test_returns_none_for_missing_key():
+def test_pick_returns_none_for_missing_key():
 
     ctx = {'key': 'value'}
     paths = ('foo', 'foo.baz', 'foo.baz.bar')
 
     for p in paths:
-        res = u.pick(ctx, p)
+        res = pick(ctx, p)
         assert res is None
 
 
-def test_returns_proper_nested_dict_value():
+def test_pick_returns_proper_nested_dict_value():
 
     ctx = {'foo': {'baz': 'bar'}}
     path = 'foo.baz'
 
-    assert u.pick(ctx, path) == 'bar'
+    assert pick(ctx, path) == 'bar'
 
 
-def test_returns_attr_value():
+def test_pick_returns_attr_value():
 
     Obj = namedtuple('Obj', ['bar'])
     o = Obj(bar='test')
@@ -31,4 +31,4 @@ def test_returns_attr_value():
     ctx = {'foo': {'baz': o}}
     path = 'foo.baz.bar'
 
-    assert u.pick(ctx, path) == 'test'
+    assert pick(ctx, path) == 'test'
